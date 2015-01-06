b63b5a0281b74a8c096f5934b8be35483a7289e2
perlancar (@pc-office) <perlancar@gmail.com>
2015-01-06 14:20:56 +0700

    Initial commit

---
+++ lib/Perl/Examples.pm
@@ -0,0 +1,37 @@
+package Perl::Examples;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: A collection of various examples of Perl modules/scripts
+
+=head1 DESCRIPTION
+
+B<EARLY RELEASE, MORE EXAMPLES TO COME, CONTRIBUTIONS WELCOME.>
+
+This distribution, as the name suggests, contains a collection of various
+examples of Perl modules/scripts. If you happen to need a script/module that:
+
+=over
+
+=item * contains some warnings and errors
+
+=item * downloads some stuffs from the Internet using {LWP,HTTP::Tiny,...}
+
+=item * contains an endless loop
+
+=item * creates a circular data structure
+
+=item * generates some random text
+
+=item * do some busy work for X seconds/minutes
+
+=item * demonstrates some Perl features
+
+=item * and so on
+
+=back
+
+but don't want to have to write it yourself, then this distribution is for you.
+
