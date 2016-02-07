33cc7b2715b0d040bfade49a16d91ba4cd775ae1
perlancar (@pc-home) <perlancar@gmail.com>
2016-02-07 21:46:49 +0700

    Add module: Perl::Examples::POD::Link

---
+++ lib/Perl/Examples/POD/Link.pm
@@ -0,0 +1,38 @@
+package Perl::Examples::POD::Link;
+
+1;
+# ABSTRACT: Show the various examples of links
+
+=head1 DESCRIPTION
+
+=head2 Links to POD
+
+Link to module: L<Perl::Examples>
+
+Link to module + section: L<Perl::Examples/"DESCRIPTION">
+
+Link this page + section: L</"URL links">
+
+Link to module with text: L<The Perl-Examples main module|Perl::Examples>
+
+Link to module with text + section: L<The description of Perl-Examples main module|Perl::Examples/"DESCRIPTION">
+
+Link this page with text + section: L<A collection of URL links/"URL links">
+
+Link to script: L<perl-example-die>
+
+=head2 URL links
+
+URL without text: L<http://www.example.com/page.html>
+
+URL with text: L<An example page|http://www.example.com/page.html>
+
+http scheme with port: L<http://www.example.com:8001/>
+
+https scheme: L<https://www.example.com/>
+
+ftp scheme: L<ftp://ftp.example.com/>
+
+mailto scheme: L<mailto:example@example.com>
+
+Some custom scheme: L<foo://bar>
